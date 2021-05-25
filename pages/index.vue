12c48eba4448a6480adf5bf090e9c07821b3be21
Paul Anthony McGowan <xhostcom@gmail.com>
2021-05-25 18:42:38 +0100

    First Commit

---
+++ pages/index.vue
@@ -0,0 +1,88 @@
+<template>
+ <div>
+     <section class="hero is-medium" :style="{ backgroundImage: `url(${hero})` }">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title"> Celebrate Uniquesness</h1>
+          <h2 class="is-size-3 has-text-weight-light">
+            Beautiful mobile covers that <br>
+            reflects your personality.
+          </h2>
+        </div>
+      </div>
+    </section>
+    <section class="section category-list">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-one-fifth">
+            <div class="box">
+              <!--<h2 class="subtitle has-text-weight-bold has-text-black">Filter by type:</h2>
+              <div class="field" v-for="type in types" :key="type">
+                <b-checkbox v-model="selectedTypes"
+                  :native-value="type" class="is-warning"> {{ type }}</b-checkbox>
+              </div>-->
+            </div>
+          </div>
+          <div class="column">
+            <div class="columns is-multiline is-variable is-2">
+              <div class="column is-one-third" v-for="product in products" :key="product._id">
+                <product-item :product="product"></product-item>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  <section class="section">
+    <div class="columns is-mobile">
+      <card
+        title="Free"
+        icon="github"
+      >
+        Open source on <a href="https://github.com/buefy/buefy">
+          GitHub
+        </a>
+      </card>
+
+      <card
+        title="Responsive"
+        icon="cellphone-link"
+      >
+        <b class="has-text-grey">
+          Every
+        </b> component is responsive
+      </card>
+
+      <card
+        title="Modern"
+        icon="alert-decagram"
+      >
+        Built with <a href="https://vuejs.org/">
+          Vue.js
+        </a> and <a href="http://bulma.io/">
+          Bulma
+        </a>
+      </card>
+
+      <card
+        title="Lightweight"
+        icon="arrange-bring-to-front"
+      >
+        No other internal dependency
+      </card>
+    </div>
+  </section>
+  </div>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  }
+}
+</script>
